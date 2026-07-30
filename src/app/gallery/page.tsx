62e2fa55052aa6773b05fcca7a277dce85fbc909
Mukwevho Henry <84884688+Henryrsa@users.noreply.github.com>
2026-07-30 15:33:29 +0200

    Replace video 4 with new reel 1988303598478147

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -25,9 +25,9 @@ const videos = [
     title: "Full Build",
   },
   {
-    url: "https://www.facebook.com/reel/788862933605298/",
+    url: "https://www.facebook.com/reel/1988303598478147/",
     embedUrl:
-      "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F788862933605298%2F&show_text=false&width=267",
+      "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1988303598478147%2F&show_text=false&width=267",
     title: "Full Build",
   },
 ];
